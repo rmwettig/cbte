00312b4243887f4114922afbcbb8b196d1e754cd
MW <N/A>
2016-09-19 11:34:43 +0200

    add unity references and change to delegate

--- TerrainTool/Assets/Editor/CBTE/Events/ConstraintChangedEvent.cs
+++ TerrainTool/Assets/Editor/CBTE/Events/ConstraintChangedEvent.cs
@@ -1,15 +1,4 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-public class ConstraintChangedEvent
-{
-	public virtual void ConstraintChanged(Constraint constraint)
-	{
-		throw new System.NotImplementedException();
-	}
-
-}
+public delegate void ConstraintChangedEvent(Constraint constraint);
 
