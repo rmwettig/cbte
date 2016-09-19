038724d5a9107d77cb0fa61306ff250211677c33
MW <N/A>
2016-09-19 12:11:50 +0200

    drop autogenerated code

--- TerrainTool/Assets/CBTE/ConstraintsContext.cs
+++ TerrainTool/Assets/CBTE/ConstraintsContext.cs
@@ -1,59 +1,79 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Represents the user access point to control active constraints
+/// </summary>
+[ExecuteInEditMode]
 public class ConstraintsContext : MonoBehaviour
 {
-	private Terrain _terrain
+    private Terrain _terrain;
+    private TextureSourceManager _tsm;
+    [SerializeField]
+    private List<Constraint> _constraints = new List<Constraint>(1);
+    private bool _noError = false;
+	public TextureSourceManager TextureSourceManager
 	{
-		get;
-		set;
+        get { return _tsm; }
 	}
 
-	public virtual TextureSourceManager TextureSourceManager
+    private void OnEnable()
+    {
+        _terrain = GetComponent<Terrain>();
+        if (_terrain == null)
+        {
+            Debug.LogError("Missing Terrain! Constraints cannot be used without a Unity terrain.");
+        }
+        else
+        {
+            if (_tsm == null)
+            {
+                _tsm = ScriptableObject.CreateInstance<TextureSourceManager>();
+            }
+            _noError = true;
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+
+    }
+    /// <summary>
+    /// Render constraint representations in the scene view
+    /// </summary>
+    public void OnDrawGizmosSelected()
 	{
-		get;
-		set;
+        if (!_noError) return;
+		foreach(Constraint c in _constraints)
+        {
+            c.DrawGizmo();
+        }
 	}
 
-	public virtual IEnumerable<Constraint> Constraint
-	{
-		get;
-		set;
-	}
+    #region EventHandler
 
-	public virtual void OnConstraintUp(Constraint constraint)
-	{
-		throw new System.NotImplementedException();
-	}
+    public virtual void OnConstraintUp(Constraint constraint)
+    {
+        throw new System.NotImplementedException();
+    }
 
     public virtual void OnConstraintDown(Constraint constraint)
-	{
-		throw new System.NotImplementedException();
-	}
+    {
+        throw new System.NotImplementedException();
+    }
 
     public virtual void OnConstraintDelete(Constraint constraint)
-	{
-		throw new System.NotImplementedException();
-	}
+    {
+        throw new System.NotImplementedException();
+    }
 
     public virtual void OnConstraintFrozen(Constraint constraint)
-	{
-		throw new System.NotImplementedException();
-	}
+    {
+        throw new System.NotImplementedException();
+    }
 
-	public virtual void OnSceneGUI()
-	{
-		throw new System.NotImplementedException();
-	}
-
-	public virtual void OnInspectorGUI()
-	{
-		throw new System.NotImplementedException();
-	}
+    #endregion
 
 }
 
