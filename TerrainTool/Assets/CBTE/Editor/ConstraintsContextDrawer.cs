f493465d044808d000243ccef7473d0fe8a3299c
MW <N/A>
2016-09-19 11:46:01 +0200

    add context drawer stub

---
+++ TerrainTool/Assets/CBTE/Editor/ConstraintsContextDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ConstraintsContext))]
+public class ConstraintsContextDrawer : Editor
+{
+
+    private void OnEnable()
+    {
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+
+    }
+
+    private void OnSceneGUI()
+    {
+        
+    }
+}
