e591e462dc30e14a30f6c125986c484658852b35
MW <N/A>
2016-09-21 10:50:34 +0200

    remove reference counter

--- TerrainTool/Assets/CBTE/TextureSources/TextureSourceManager.cs
+++ TerrainTool/Assets/CBTE/TextureSources/TextureSourceManager.cs
@@ -5,7 +5,6 @@ public class TextureSourceManager : ScriptableObject
 {
     private TextureSource[] _prototypes = null;
     private TextureSource[] _textureSources = null;
-    private Dictionary<TextureSource, int> _referenceCount = null;
 
 	public TextureSource FindSourceByName(string name)
 	{
@@ -40,23 +39,11 @@ public class TextureSourceManager : ScriptableObject
     public void OnEnable()
     {
         _textureSources = new TextureSource[1];
-        _referenceCount = new Dictionary<TextureSource, int>();
     }
 
     public void AddPrototypes(params TextureSource[] prototypes)
     {
         _prototypes = prototypes;
-    }
-
-    private TextureSource[] RebuildSources()
-    {
-        TextureSource[] arr = new TextureSource[_referenceCount.Keys.Count];
-        int i = 0;
-        foreach(KeyValuePair<TextureSource, int> kv in _referenceCount)
-        {
-            arr[i++] = kv.Key;
-        }
-        return arr;
     }
 }
 
