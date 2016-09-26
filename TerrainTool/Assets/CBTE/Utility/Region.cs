5f0bfcd1ade4b3522ff7e706df95a8fb88e9abe3
MW <N/A>
2016-09-26 10:46:14 +0200

    add Region stub

---
+++ TerrainTool/Assets/CBTE/Utility/Region.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Holds affected terrain heightmap section coordinates
+/// </summary>
+public class Region : ScriptableObject
+{
+    private int _x = 0;
+    private int _y = 0;
+    private int _xCount = 0;
+    private int _yCount = 0;
+
+    public int X { get { return _x; } }
+    public int Y { get { return _y; } }
+    public int XCount { get { return _xCount; } }
+    public int YCount { get { return _yCount; } }
+
+    public static Region CalculateRegion(Vector3 position, Terrain terrain)
+    {
+
+    }
+}
