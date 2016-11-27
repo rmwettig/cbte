a7cf1174c7a51933161d5ab2d2fcb85320b196da
MW <N/A>
2016-11-27 18:33:02 +0100

    add unit tests

---
+++ TerrainTool/Assets/Editor/UnitTests/RegionTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class RegionTest {
+
+	[Test]
+	public void CalculateRegionWhenAtZeroPositionTest()
+	{
+        //create a 100x100 terrain with a 513x513 heightmap
+        TerrainData td = new TerrainData();
+        td.size = new Vector3(500f, 0f, 500f);
+        td.heightmapResolution = 513;
+        GameObject go = Terrain.CreateTerrainGameObject(td);
+        go.transform.position = Vector3.zero;
+        Terrain terrain = go.GetComponent<Terrain>();
+        //assume constraint being 100x100 units large
+        float width = 100f;
+        float length = 100f;
+        //assume being at zero position taking into account the dimensions
+        Vector3 position = new Vector3(50f, 0, 50f);
+        
+        Region r = Region.CalculateRegion(position, width, length, terrain);
+
+        //assumptions:
+        //looking onto the x-z plane (along y downwards)
+        //z points to right
+        //x points down
+        //thus, the affected heightmap region must have the following values:
+        //x-start: (pos.x - width/2)/terrain.size.x *hmreso == (50 - 50)/500 *513 = 0
+        //y-start: (pos.y - length/2)/terrain.size.y *hmreso == (50 - 50)/500 *513 = 0
+        //x-end: ((xStart + (pos.x + width/2))/terrain.size.x)*hmresolution == ((0 + (50 + 50))/500)*513 = 100/500*513 = 0.2*513 = 102,6 -> 102
+        //y-end: ((yStart + (pos.y + length/2))/terrain.size.y)*hmresolution == ((0 + (50 + 50))/500)*513 = 100/500*513 = 0.2*513 = 102,6 -> 102
+        //xcount: xend - xstart = 102 - 0 = 102
+        //ycount: yend - ystart = 102 - 0 = 102
+        Assert.AreEqual(0, r.X);
+        Assert.AreEqual(0, r.Y);
+        Assert.AreEqual(102, r.XCount);
+        Assert.AreEqual(102, r.YCount);
+
+	}
+}
---
+++ TerrainTool/Assets/Editor/UnitTests/UtilityTest.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class UtilityTest
+{
+
+    [Test]
+    public void CalculateLimitsWithinBounds()
+    {
+        //assume a position of 200u at any axis
+        float position = 200f;
+        float min, max;
+        Utility.CalculateLimits(100f, position, 0f, 500f, out min, out max);
+
+        Assert.AreEqual(150f, min);
+        Assert.AreEqual(250f, max);
+    }
+
+    [Test]
+    public void CalculateLimitsExceedingLowerBound()
+    {
+        //assume a position of 200u at any axis
+        float position = 40f;
+        float min, max;
+        Utility.CalculateLimits(100f, position, 0f, 500f, out min, out max);
+
+        //expectations:
+        //left limit: pos - dimension/2 = 40f - 50f = -10f -> 0f;
+        //upper limit: pos + dimension/2 = 40f + 50f = 90f
+
+        Assert.AreEqual(0f, min);
+        Assert.AreEqual(90f, max);
+    }
+
+    [Test]
+    public void CalculateLimitsExceedingUpperBound()
+    {
+        //assume a position of 200u at any axis
+        float position = 460f;
+        float min, max;
+        Utility.CalculateLimits(100f, position, 0f, 500f, out min, out max);
+
+        //expectations:
+        //left limit: pos - dimension/2 = 460f - 50f = 410f
+        //upper limit: pos + dimension/2 = 460f + 50f = 510f -> 500f
+
+        Assert.AreEqual(410f, min);
+        Assert.AreEqual(500f, max);
+    }
+
+    [Test]
+    public void CalculateLimitsExceedingBothBounds()
+    {
+        //assume a position of 200u at any axis
+        float position = 40;
+        float min, max;
+        Utility.CalculateLimits(100f, position, 0f, 80f, out min, out max);
+
+        //expectations:
+        //left limit: pos - dimension/2 = 40 - 50f = -10 -> 0
+        //upper limit: pos + dimension/2 = 40 + 50f = 90 -> 80
+
+        Assert.AreEqual(0f, min);
+        Assert.AreEqual(80f, max);
+    }
+}
